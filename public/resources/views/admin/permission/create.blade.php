74276bd9b59cf8ef4542c57826ea439042c9c784
lilong <lilong@dgg.net>
2018-01-11 15:32:25 +0800

    rm

--- public/resources/views/admin/permission/create.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('admin.base')
-
-@section('content')
-    <div class="layui-elem-quote">添加权限</div>
-    <form class="layui-form" action="{{route('permission.store')}}" method="post">
-        @include('admin.permission._from')
-    </form>
-@endsection
--- public/resources/views/admin/role/create.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('admin.base')
-
-@section('content')
-    <div class="layui-elem-quote">添加角色</div>
-    <form action="{{route('role.store')}}" method="post" class="layui-form">
-        @include('admin.role._form')
-    </form>
-@endsection
